164c7207374131ece23a8ddf40c7a7981f3c7084
marksev1 <marksev1@gmail.com>
2016-09-06 10:39:18 +0200

    Attempt at modifying the news skill for my locale
    As the title implies.

--- mycroft/skills/slo_news/__init__.py
+++ mycroft/skills/slo_news/__init__.py
@@ -26,22 +26,22 @@ from mycroft.skills.core import MycroftSkill
 from mycroft.util import play_mp3
 from mycroft.util.log import getLogger
 
-__author__ = 'jdorleans'
+__author__ = 'jdorleans, msev'
 
 LOGGER = getLogger(__name__)
 
 
-class NPRNewsSkill(MycroftSkill):
+class SLONewsSkill(MycroftSkill):
     def __init__(self):
-        super(NPRNewsSkill, self).__init__(name="NPRNewsSkill")
+        super(SLONewsSkill, self).__init__(name="SLONewsSkill")
         self.url_rss = self.config['url_rss']
         self.weather = weather.WeatherSkill()
         self.process = None
 
     def initialize(self):
         self.load_data_files(dirname(__file__))
-        intent = IntentBuilder("NPRNewsIntent").require(
-            "NPRNewsKeyword").build()
+        intent = IntentBuilder("SLONewsIntent").require(
+            "SLONewsKeyword").build()
         self.register_intent(intent, self.handle_intent)
 
         self.weather.bind(self.emitter)
@@ -50,7 +50,7 @@ class NPRNewsSkill(MycroftSkill):
     def handle_intent(self, message):
         try:
             data = feedparser.parse(self.url_rss)
-            self.speak_dialog('npr.news')
+            self.speak_dialog('slo.news')
             time.sleep(3)
             self.process = play_mp3(data['entries'][0]['links'][0]['href'])
 
@@ -59,10 +59,10 @@ class NPRNewsSkill(MycroftSkill):
 
     def stop(self):
         if self.process and self.process.poll() is None:
-            self.speak_dialog('npr.news.stop')
+            self.speak_dialog('slo.news.stop')
             self.process.terminate()
             self.process.wait()
 
 
 def create_skill():
-    return NPRNewsSkill()
+    return SLONewsSkill()
